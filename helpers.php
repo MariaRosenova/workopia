6cf77938c4bbb9ade9d8d79d098de601e0a4607d
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-12-26 22:05:07 +0200

    Add helper funcs

--- helpers.php
+++ helpers.php
@@ -6,6 +6,38 @@
  * @return string
  */
 
- function basePath($path = '') {
-     return __DIR__ . '/..'. ($path ? DIRECTORY_SEPARATOR . $path : $path);
- }
+function basePath($path = '')
+{
+    return __DIR__ . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+}
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void  
+ */
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (!file_exists($viewPath)) {
+        throw new Exception("View {$name} not found at {$viewPath}");
+    }
+    require $viewPath;
+}
+
+
+/**
+ * Load a partial
+ * @param string $name
+ * @return void  
+ */
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (!file_exists($partialPath)) {
+        throw new Exception("Partial {$name} not found at {$partialPath}");
+    }
+    require  $partialPath;
+}
